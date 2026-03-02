28cf7f7657afaea866ce8ad2b38235a3f2d95bfb
leannefuentes23 <leanne.fuentes@upr.edu>
2026-03-02 18:53:42 -0400

    Add second DyEnemy texture

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -12,12 +12,19 @@ private:
     float aimAngle = 225;
     bool loop = false;
 
+    // Create variable to store random texture 
+    Rectangle randomTexture;
+
 public:
     int scoreValue = 160;
     DyEnemy(float x, float y) : Enemy(x, y)
     {
         this->cooldown = GetRandomValue(90, 300);
         this->health = 1;
+
+        // Construct DyEnemy with a random texture 
+        this->randomTexture = GetRandomValue(0,1) == 0 ? Rectangle{2, 128, 13, 14} : Rectangle{2, 147, 13, 13};
+        
     }
 
     void draw() override;
